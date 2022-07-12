0b90ec63d80864b9a4ba3f655a0f451d634ba39e
zzinao <kjn0265@gmail.com>
2022-07-13 03:01:58 +0900

    js doc을 통해 타입 설정해보기

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,13 +1,31 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
 
 // user data
 var user = {};
+// 아래 js doc의 기본 문법
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {string} address
+ */
+
+/**
+ *
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+console.log(fetchUser());
 
 function startApp() {
   axios
@@ -15,7 +33,11 @@ function startApp() {
     .then(function (response) {
       console.log(response);
       user = response.data;
+      console.log(user);
       // TODO: 이름, 이메일, 주소 표시하기
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.city;
     })
     .catch(function (error) {
       console.log(error);
