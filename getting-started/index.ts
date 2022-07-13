820effe519ac7f6bc6c843d0784c9e54042663ba
zzinao <kjn0265@gmail.com>
2022-07-14 01:37:09 +0900

    tsc 실행

--- getting-started/index.ts
+++ getting-started/index.ts
@@ -1,7 +1,7 @@
-function add(a: number, b: number): number {
+function addd(a: number, b: number): number {
   return a + b;
 }
 
-add(10, 20);
+addd(10, 20);
 
 // tsc index.ts 를 실행하면 js로 변환해 준다. 하지만 일일히 변환해 주기 번거롭기 때문에 웹팩으로 자동화 설정을 해주면 좋다.
